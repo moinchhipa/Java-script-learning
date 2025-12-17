07dec07f6ac59939c593ac45d6e109dffa42528d
Moin Chhipa <183844907+moinchhipa@users.noreply.github.com>
2025-12-17 18:59:43 +0530

    Added Downloadable option and styled for responsive

--- resume_builder/script.js
+++ resume_builder/script.js
@@ -1,10 +1,10 @@
-function bindInput(inputId, prevId, placeHolder){
-    const input = document.getElementById(inputId);
-    const preview = document.getElementById(prevId);
+function bindInput(inputId, prevId, placeHolder) {
+  const input = document.getElementById(inputId);
+  const preview = document.getElementById(prevId);
 
-    input.addEventListener("input", () => {
-        preview.textContent = input.value.trim() || placeHolder;
-    })
+  input.addEventListener("input", () => {
+    preview.textContent = input.value.trim() || placeHolder;
+  });
 }
 
 bindInput("nameInput", "prevName", "Your Name");
@@ -16,4 +16,32 @@ bindInput("educationInput", "prevEducation", "Add your education ...");
 bindInput("skillsInput", "prevSkills", "Write skills ...");
 bindInput("projectsInput", "prevProjects", "Write projects ...");
 bindInput("otherInput", "prevOther", "Write other info ...");
+
+document.getElementById("Download_btn").addEventListener("click", () => {
+  document.body.classList.add("print-mode");
+
+  const resume = document.getElementById("resume");
+
+  const opt = {
+  margin: 0,
+  filename: "My_Resume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: {
+    scale: 2,
+    scrollY: 0,
+    scrollX: 0,
+    useCORS: true
+  },
+  jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+};
+
+
+  html2pdf()
+    .from(resume)
+    .set(opt)
+    .save()
+    .then(() => {
+      document.body.classList.remove("print-mode");
+    });
+});
 
